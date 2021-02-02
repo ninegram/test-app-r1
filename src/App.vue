2f8df13f9f90f35724564bff8f079af7d3199a56
Euiyeon Kim <ppang327@gmail.com>
2021-02-03 05:40:50 +0900

    add test page

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,59 @@
 <template>
   <div id="app">
     <b-container>
-      <h1>Hello App!</h1>
-      <p>
+      <h1 class="text-muted mb-4">ENNEAGRAM HERA</h1>
+      <!-- <p> -->
         <!-- 네비게이션을 위해 router-link 컴포넌트를 사용합니다. -->
         <!-- 구체적인 속성은 `to` prop을 이용합니다. -->
         <!-- 기본적으로 `<router-link>`는 `<a>` 태그로 렌더링됩니다.-->
-        <router-link to="/hello-vue">Hello World</router-link>
-        <router-link to="/b-icon">BIcon Test Page</router-link>
-      </p>
+        <!-- <router-link to="/hello-vue">Hello World</router-link>
+        <router-link to="/b-icon">BIcon Test Page</router-link> -->
+      <!-- </p> -->
       <!-- 라우트 아울렛 -->
       <!-- 현재 라우트에 맞는 컴포넌트가 렌더링됩니다. -->
-      <router-view></router-view>
+      <!-- <router-view></router-view> -->
+      
+      <b-card no-body>
+        <b-tabs pills card vertical>
+          <b-tab title="시작하기" active>
+              <tab-page-1></tab-page-1>
+            </b-tab>
+          <b-tab title="유의사항">
+            <tab-page-2></tab-page-2>
+          </b-tab>
+          <b-tab title="그룹 1">
+            <tab-page-3></tab-page-3>
+          </b-tab>
+          <b-tab title="그룹 2">
+            <tab-page-4></tab-page-4>
+          </b-tab>
+          <b-tab title="중간결과">
+            <tab-page-5></tab-page-5>
+          </b-tab>
+        </b-tabs>
+      </b-card>
     </b-container>
   </div>
 </template>
 
 <script>
+import TabPage1 from './components/eg-test/page1'
+import TabPage2 from './components/eg-test/page2'
+import TabPage3 from './components/eg-test/page3'
+import TabPage4 from './components/eg-test/page4'
+import TabPage5 from './components/eg-test/page5'
 
 export default {
   name: 'App',
   components: {
+    TabPage1,
+    TabPage2,
+    TabPage3,
+    TabPage4,
+    TabPage5,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/eg-test/page5/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <b-card-text class="tab-card">
+    <h3 class="m-5 text-muted">
+      <strong>ENNEAGRAM</strong>HERA
+    </h3>
+    <h3 class="m-5 text-muted">
+      중간결과
+    </h3>
+    <div class="result" style="margin-top: 1em;">
+      <p>
+        당신은 <strong>7유형</strong>일 가능성이 높습니다.<br/>
+        정확한 유형 분석을 위해 <br/>
+        다음 페이지 테스트에서<br/>
+        해당되는 항목에 모두 체크해주세요.
+      </p>
+
+    </div>
+    
+    <b-button class="m-5" size="lg" type="submit" variant="primary">계속하기</b-button>
+  </b-card-text>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  
+}
+</script>
+
+<style scoped lang="scss">
+.tab-card {
+  text-align: center;
+  .result > p {
+    font-size: 1.5em;
+    line-height: 2em;
+  }
+}
+</style>
